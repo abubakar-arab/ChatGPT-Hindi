40d7d4518066a563cec6df00b169c8a896f48ca9
Abubakar Arab <abubakararab123@gmail.com>
2022-12-20 18:45:07 +0530

    Added layout for Sidebar and Main container/ Work in progress for buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import "./App.css";
+import ChatGPT from "./components/ChatGPT";
+import SideBar from "./components/SideBar.js";
+import bootstrap from 'bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <p>ChatGPT Hindi</p>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      <div className="sidebar">
+          <SideBar/>
+          </div>
+        <header className="App-header">
+          <p>ChatGPT Hindi</p>
+          <ChatGPT />
+        </header>
+        
+      </div>
+    </>
   );
 }
 
---
+++ src/components/SideBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ButtonS from "./ButtonS";
+
+const SideBar = () => {
+  return (
+    <div className="flex container">
+          <ButtonS />
+        
+    </div>
+  );
+};
+
+export default SideBar;
