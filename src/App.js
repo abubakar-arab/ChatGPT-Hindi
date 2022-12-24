053cd7281ec88333e039ca213db7da0c9683d151
Abubakar Arab <abubakararab123@gmail.com>
2022-12-24 17:48:29 +0530

    Added bot loading animation and typing animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import "./App.css";
-import ChatGPT from "./components/ChatGPT";
 import SideBar from "./components/SideBar.js";
-import bootstrap from 'bootstrap';
+import bootstrap from "bootstrap";
+import sendLogo from "./assets/send.svg"
+
+
+
 
 function App() {
+  let loadInterval;
+  // Function for loading animation of bot
+  function loader(element){
+    element.textContent = "";
+    loadInterval = setInterval(() => {
+      element.textContent += ".";
+      if(element.textContent === "...."){
+        element.textContent = "";
+      }
+    }, 300)
+  }
+  // Function for typing animation of bot
+  function typeText(element, text){
+    let index = 0;
+    let interval = setInterval(()=>{
+      if(index<text.length){
+        element.innerHTML += text.charAt(index);
+
+      }else{
+        clearInterval(interval);
+      }
+    },20)
+
+  }
+
   return (
     <>
-      <div className="App">
-      <div className="sidebar">
-          <SideBar/>
-          </div>
-        <header className="App-header">
+      <div className="app">
+        {/* <header className="App-header">
           <p>ChatGPT Hindi</p>
-          <ChatGPT />
-        </header>
-        
+        </header> */}
+        <div className="chat_container">
+          <form>
+            <textarea
+              name="prompt"
+              cols="1"
+              rows="1"
+              placeholder="Ask Codebuddy..."
+            ></textarea>
+            <button type="submit">
+              <img src={sendLogo} alt="Send" />
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
 }
+
 
 export default App;
