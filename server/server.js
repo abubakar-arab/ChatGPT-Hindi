5904d686357d46ff23cc328375b7ab25c6989b4e
Abubakar Arab <abubakararab123@gmail.com>
2022-12-27 17:32:13 +0530

    Fixed the issue where object was going as a prompt to OpenAI server

--- server/server.js
+++ server/server.js
@@ -1,51 +1,39 @@
 import express from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import * as dotenv from "dotenv";
+import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
-
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-
+const port = 5000;
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.get('/', async (req, res) => {
-    res.status(200).send({
-        message:"Hello from codex",
-    })
+app.post("/", async (req, res) => {
+  const message = req.body;/// This is prompt coming from UI written by user as a message.
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${message.message}`,
+    temperature: 0,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0.5,
+    presence_penalty: 0,
+  });
+  res.json({
+    // data:response.data
+    message: response.data.choices[0].text,
+  });
 });
 
-app.post('/', async (req, res) => {
-    try{
-        const prompt = req.body.prompt;
 
-        const response = openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 256,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0
-        })
+app.listen(port, () => console.log(`Server is running on port http://localhost:${port}`));
 
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-
-    }catch(error){
-        console.log(error);
-        res.status(500).send({error});
-
-    }
-} )
-
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
